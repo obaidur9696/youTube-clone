9aa412725dbf63b1ee81da9387d96db754dde4b0
Obaidur Rahman <obaidur9696@gmail.com>
2026-01-29 23:33:42 +0530

    feat(channel): implement getUserChannelProfile function to fetch user channel details with subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -383,6 +383,87 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
 
 })
 
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+   const { username } = req.params
+
+   if (!username?.trim()) {
+      throw new ApiError(400, "Username is missing when getUserChannelProfile called")
+   }
+
+   const channel = await User.aggregate([
+      {
+         $match: {
+            username: username?.toLowerCase()
+         }
+      },
+      {
+         $lookup: {
+            from: "subscriptions",
+            localField: _id,
+            foreignField: "channel",
+            as: "subscribers"
+         },
+
+         $lookup: {
+            from: "subscriptions",
+            localField: _id,
+            foreignField: "subscriber",
+            as: "subscribedTo"
+         }
+      },
+
+      {
+         $addFields: {
+            subscribersCount: {
+               $size: "$subscribers"
+            },
+            channelsSubscribedToCount: {
+               $size: "subscribedTo"
+            },
+
+            //checked isSubscribed or not?
+            isSubscribed: {
+               $cond: {
+                  if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                  then: true,
+                  esle: false
+               }
+            }
+         }
+      },
+
+      {
+         $project: {
+            username: 1,
+            fullName: 1,
+            email: 1,
+            coverImage: 1,
+            avatar: 1,
+            subscribersCount,
+            channelsSubscribedToCount,
+            isSubscribed
+
+         }
+      }
+
+   ])
+
+   if (!channel?.length) {
+      throw new ApiError(400, "Channel does not exits")
+   }
+
+   return res
+      .status(200)
+      .json(
+         new ApiResponse(
+            200,
+            channel[0],
+            "User channel fetched successfully"
+         )
+      )
+})
+
 export {
    registerUser,
    loginUser,
@@ -392,5 +473,6 @@ export {
    getCurrentUser,
    updateAccountDetails,
    updateUserAvatar,
-   updateUserCoverImage
+   updateUserCoverImage,
+   getUserChannelProfile
 }
